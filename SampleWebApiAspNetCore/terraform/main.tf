6f55a4b28f3c637ab7585828883c25aa3ebeb614
vasylky <vasylqvky@gmail.com>
2025-04-23 13:40:30 +0300

    ll

--- SampleWebApiAspNetCore/terraform/main.tf
+++ SampleWebApiAspNetCore/terraform/main.tf
@@ -37,14 +37,14 @@ resource "azurerm_linux_web_app" "main" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-example"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-example"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
